a694a6434ef7fd68e69cbd0a271a6f795f758210
ak4391076 <ak4391076@gmail.com>
2026-06-28 11:18:50 +0300

    remove envDir property from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/blog-preview-card-solution/",
-  envDir: false,
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
