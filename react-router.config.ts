d6f5f653a73d3accc5dfb86afe2ff08a0d161d92
ak4391076 <ak4391076@gmail.com>
2026-06-28 13:10:46 +0300

    update configs

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  basename: "/blog-preview-card-solution",
+  basename: "/blog-preview-card-solution/",
   ssr: false,
   prerender: true,
 } satisfies Config;
