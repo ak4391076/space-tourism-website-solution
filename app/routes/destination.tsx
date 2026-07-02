1a511f1d042e78ae02e653470c4f04bd5e6fa60a
ak4391076 <ak4391076@gmail.com>
2026-07-02 19:51:57 +0300

    assign destination name to image alt attribute

--- app/routes/destination.tsx
+++ app/routes/destination.tsx
@@ -32,7 +32,7 @@ export const Destination = () => {
                 <img
                   className="h-auto max-w-37.5 md:max-w-75 lg:max-w-full my-6.5 md:my-10.5"
                   src={`${import.meta.env.BASE_URL}${destinationState.images.png}`}
-                  alt=""
+                  alt={destinationState.name}
                 />
               </picture>
             </div>
